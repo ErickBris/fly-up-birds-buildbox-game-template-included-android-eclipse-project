356a1bd6eb32a66b19fcd37717a175d051954889
ErickBris <erick1saradmon@gmail.com>
2022-05-08 11:10:02 -0500

    Initial commit

---
+++ Fly Up Birds/android/gen/com/schemestudios/flyupbirds/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.schemestudios.flyupbirds;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# fly-up-birds-buildbox-game-template-included-android-eclipse-project
+ fly-up-birds-buildbox-game-template-included-android-eclipse-project
